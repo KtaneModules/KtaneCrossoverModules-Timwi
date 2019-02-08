f35b787a28b8ac0f50eed2b42b8ab02900fdd6f2
Timwi <github@timwi.de>
2019-02-09 00:40:29 +0100

    Complicated Buttons can now be striped red/blue

--- Assets/Modules/Complicated Buttons/ComplicatedButtonsModule.cs
+++ Assets/Modules/Complicated Buttons/ComplicatedButtonsModule.cs
@@ -13,15 +13,17 @@ public class ComplicatedButtonsModule : MonoBehaviour
     public KMAudio Audio;
     public KMSelectable[] Buttons;
     public TextMesh[] Labels;
-    public Material[] materials;
+    public Texture[] Textures;
 
-    static string[] words = { "Press", "Hold", "Detonate" };
-    static string[] colorNames = { "White", "Red", "Blue", "Purple" };
-    static int[,,] orders = {
+    static readonly string[] words = { "Press", "Hold", "Detonate" };
+    static readonly string[] colorNames = { "Red", "Blue", "White" };
+    static readonly int[,,] orders = {
         { { 1, 2, 3 }, { 2, 3, 1 }, { 3, 1, 2 }, { 1, 2, 3 } },
         { { 2, 1, 3 }, { 3, 2, 1 }, { 1, 3, 2 }, { 2, 3, 1 } },
         { { 3, 1, 2 }, { 1, 2, 3 }, { 2, 1, 3 }, { 3, 1, 2 } }
     };
+
+    static readonly Color[] colors = new[] { new Color32(0xff, 0x41, 0x41, 0xff), new Color32(0x41, 0x86, 0xff, 0xff), new Color32(0xff, 0xff, 0xff, 0xff) }.Select(c => (Color) c).ToArray();
 
     const string instructions = "PDPBRSDSRBPBRRSD";
 
@@ -45,12 +47,14 @@ public class ComplicatedButtonsModule : MonoBehaviour
             Labels[i].text = words[Random.Range(0, 3)];
             if (Labels[i].text == "Press") actions[i] += 2;
 
-            var color = Random.Range(0, 4);
-            Buttons[i].GetComponent<Renderer>().sharedMaterial = materials[color];
-            if (color == 1 || color == 3) actions[i] += 8;
-            if (color == 2 || color == 3) actions[i] += 4;
+            var color1 = Random.Range(0, 3);    // 0 = red, 1 = blue, 2 = white
+            var color2 = Random.Range(0, 3);
+            Buttons[i].GetComponent<Renderer>().material.mainTexture = Textures[(color1 * 3 + color2) + (i == 0 ? 0 : 9)];
+            Buttons[i].GetComponent<Renderer>().material.color = color1 == color2 ? colors[color1] : Color.white;
+            if (color1 == 0 || color2 == 0) actions[i] += 8;
+            if (color1 == 1 || color2 == 1) actions[i] += 4;
 
-            Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: {2} {3}", moduleId, i + 1, colorNames[color], Labels[i].text);
+            Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: {2} {3}", moduleId, i + 1, color1 == color2 ? colorNames[color1] : string.Format("{0}-{1}", colorNames[color1], colorNames[color2]), Labels[i].text);
         }
 
         BombModule.OnActivate += OnActivate;
@@ -107,23 +111,23 @@ public class ComplicatedButtonsModule : MonoBehaviour
                 Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: Push.", moduleId, button);
                 return true;
             case 'R':
-                {
-                    var res = BombInfo.GetSerialNumber().Distinct().Count() != BombInfo.GetSerialNumber().Length;
-                    Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: Push if duplicate serial number characters ({2}).", moduleId, button, res);
-                    return res;
-                }
+            {
+                var res = BombInfo.GetSerialNumber().Distinct().Count() != BombInfo.GetSerialNumber().Length;
+                Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: Push if duplicate serial number characters ({2}).", moduleId, button, res);
+                return res;
+            }
             case 'S':
-                {
-                    var res = BombInfo.IsPortPresent(KMBombInfoExtensions.KnownPortType.Serial);
-                    Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: Push if serial port present ({2}).", moduleId, button, res);
-                    return res;
-                }
+            {
+                var res = BombInfo.IsPortPresent(KMBombInfoExtensions.KnownPortType.Serial);
+                Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: Push if serial port present ({2}).", moduleId, button, res);
+                return res;
+            }
             case 'B':
-                {
-                    var res = BombInfo.GetBatteryHolderCount() >= 2;
-                    Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: Push if two or more battery holders ({2}).", moduleId, button, res);
-                    return res;
-                }
+            {
+                var res = BombInfo.GetBatteryHolderCount() >= 2;
+                Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: Push if two or more battery holders ({2}).", moduleId, button, res);
+                return res;
+            }
             default:
                 Debug.LogFormat("[Complicated Buttons #{0}] Button {1}: Do not push.", moduleId, button);
                 return false;
